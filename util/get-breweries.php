5d771b0aba404ea1c2b9ebaabd384e5e203045f3
Nick Braica <nick.braica@gmail.com>
2013-06-23 18:45:57 -0400

    styled tables, worked in UX for login/logout conditions on my-breweries page and brewery-list page.

--- util/get-breweries.php
+++ util/get-breweries.php
@@ -7,10 +7,21 @@
 		die("There was a problem connecting to the database");
 	} 
 
-	$sql = "SELECT * FROM `brewery_list` ORDER BY `name`";
+	$sql = "SELECT * FROM brewery_list ORDER BY name";
 	
 	if(!$result = $db->query($sql)){
 		die('There was an error running the query [' . $db->error . ']');
+	} else {
+?>
+	<div class="row header clearfix">
+		<div class="name">Brewery</div>
+		<div class="address">Address</div>
+		<div class="city">City</div>
+		<div class="state">State</div>
+		<div class="brewers-website">Website</div>
+		<!-- <div class="ba-link"><a href="<?php echo $ba_link; ?>">Beer Advocate</a></div> -->
+	</div>
+<?php
 	}
 
 	while($row = $result->fetch_assoc()) :
@@ -27,8 +38,8 @@
 		<div class="address"><?php echo $address; ?></div>
 		<div class="city"><?php echo $city; ?></div>
 		<div class="state"><?php echo $state; ?></div>
-		<div class="brewers-website"><a href="<?php echo $brewers_website; ?>">Brewery website</a></div>
-		<div class="ba-link"><a href="<?php echo $ba_link; ?>">Beer Advocate page</a></div>
+		<div class="brewers-website"><a href="<?php echo $brewers_website; ?>" target="_blank">Site</a></div>
+		<!-- <div class="ba-link"><a href="<?php echo $ba_link; ?>">Beer Advocate</a></div> -->
 	</div>
 
 <?php endwhile; ?>
