c05f32239ce6443252ed12a02a86f4f9d3c34a67
Nick Braica <nbraica@tankdesign.com>
2013-07-23 11:28:29 -0400

    added google analytics

--- includes/footer.php
+++ includes/footer.php
@@ -7,9 +7,15 @@
 	</footer>
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 	<script src="js/jquery-ui.min.js"></script>
-	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBbJuT9vMjlf6rPoKISHZQBTQ95Jxlnc5I&sensor=false">
-    </script>
+	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBbJuT9vMjlf6rPoKISHZQBTQ95Jxlnc5I&sensor=false"></script>
     <script src="js/infobubble.js"></script>
 	<script src="js/brewerykeeper.js"></script>
+
+    <script>
+        var _gaq=[['_setAccount','UA-20596099-9'],['_trackPageview']];
+        (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+        g.src='//www.google-analytics.com/ga.js';
+        s.parentNode.insertBefore(g,s)}(document,'script'));
+    </script>
 </body>
 </html>
